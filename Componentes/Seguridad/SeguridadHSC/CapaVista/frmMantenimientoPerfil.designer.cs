830a8bcc6dc977abd7236eb9f6823dedb66a7447
Danny Giancarlo Saldaña Salguero <dsaldanas1@miumg.edu.gt>
2022-02-20 13:59:05 -0600

    MantenimientoPerfilesDannySaldana

--- Componentes/Seguridad/SeguridadHSC/CapaVista/frmMantenimientoPerfil.designer.cs
+++ Componentes/Seguridad/SeguridadHSC/CapaVista/frmMantenimientoPerfil.designer.cs
@@ -29,45 +29,26 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMantenimientoPerfil));
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.gbxEstado = new System.Windows.Forms.GroupBox();
             this.btnInhabilitado = new System.Windows.Forms.RadioButton();
             this.btnHabilitado = new System.Windows.Forms.RadioButton();
-            this.btnInsertar = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.labelNombreaplicacion = new System.Windows.Forms.Label();
             this.labelIdaplicacion = new System.Windows.Forms.Label();
-            this.btnModificar = new System.Windows.Forms.Button();
-            this.btnEliminar = new System.Windows.Forms.Button();
-            this.btnLimpiar = new System.Windows.Forms.Button();
             this.perfilTabla = new System.Windows.Forms.DataGridView();
+            this.navegador1 = new DLL.nav.navegador();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.gbxEstado.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.perfilTabla)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox3
-            // 
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(349, 205);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(27, 20);
-            this.textBox3.TabIndex = 22;
-            this.textBox3.Visible = false;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(143, 111);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(212, 20);
-            this.textBox2.TabIndex = 1;
             // 
             // gbxEstado
             // 
             this.gbxEstado.BackColor = System.Drawing.Color.Transparent;
             this.gbxEstado.Controls.Add(this.btnInhabilitado);
             this.gbxEstado.Controls.Add(this.btnHabilitado);
-            this.gbxEstado.Location = new System.Drawing.Point(143, 164);
+            this.gbxEstado.Location = new System.Drawing.Point(155, 236);
             this.gbxEstado.Name = "gbxEstado";
             this.gbxEstado.Size = new System.Drawing.Size(200, 100);
             this.gbxEstado.TabIndex = 20;
@@ -85,6 +66,7 @@
             this.btnInhabilitado.Text = "Inhabilitado";
             this.btnInhabilitado.UseVisualStyleBackColor = true;
             this.btnInhabilitado.CheckedChanged += new System.EventHandler(this.btnInhabilitado_CheckedChanged);
+            this.btnInhabilitado.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnInhabilitado_MouseClick);
             // 
             // btnHabilitado
             // 
@@ -97,29 +79,13 @@
             this.btnHabilitado.Text = "Habilitado";
             this.btnHabilitado.UseVisualStyleBackColor = true;
             this.btnHabilitado.CheckedChanged += new System.EventHandler(this.btnHabilitado_CheckedChanged);
-            // 
-            // btnInsertar
-            // 
-            this.btnInsertar.Location = new System.Drawing.Point(500, 48);
-            this.btnInsertar.Name = "btnInsertar";
-            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
-            this.btnInsertar.TabIndex = 5;
-            this.btnInsertar.Text = "Insertar";
-            this.btnInsertar.UseVisualStyleBackColor = true;
-            this.btnInsertar.Click += new System.EventHandler(this.btnIngresar_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(143, 66);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 20);
-            this.textBox1.TabIndex = 0;
+            this.btnHabilitado.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnHabilitado_MouseClick);
             // 
             // labelNombreaplicacion
             // 
             this.labelNombreaplicacion.AutoSize = true;
             this.labelNombreaplicacion.BackColor = System.Drawing.Color.Transparent;
-            this.labelNombreaplicacion.Location = new System.Drawing.Point(38, 111);
+            this.labelNombreaplicacion.Location = new System.Drawing.Point(50, 183);
             this.labelNombreaplicacion.Name = "labelNombreaplicacion";
             this.labelNombreaplicacion.Size = new System.Drawing.Size(73, 13);
             this.labelNombreaplicacion.TabIndex = 17;
@@ -129,69 +95,74 @@
             // 
             this.labelIdaplicacion.AutoSize = true;
             this.labelIdaplicacion.BackColor = System.Drawing.Color.Transparent;
-            this.labelIdaplicacion.Location = new System.Drawing.Point(38, 73);
+            this.labelIdaplicacion.Location = new System.Drawing.Point(50, 145);
             this.labelIdaplicacion.Name = "labelIdaplicacion";
             this.labelIdaplicacion.Size = new System.Drawing.Size(47, 13);
             this.labelIdaplicacion.TabIndex = 16;
             this.labelIdaplicacion.Text = "ID Perfil:";
             // 
-            // btnModificar
-            // 
-            this.btnModificar.Location = new System.Drawing.Point(500, 77);
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(75, 23);
-            this.btnModificar.TabIndex = 6;
-            this.btnModificar.Text = "Modificar";
-            this.btnModificar.UseVisualStyleBackColor = true;
-            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Location = new System.Drawing.Point(500, 111);
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
-            this.btnEliminar.TabIndex = 7;
-            this.btnEliminar.Text = "Eliminar";
-            this.btnEliminar.UseVisualStyleBackColor = true;
-            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
-            // 
-            // btnLimpiar
-            // 
-            this.btnLimpiar.Location = new System.Drawing.Point(500, 140);
-            this.btnLimpiar.Name = "btnLimpiar";
-            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
-            this.btnLimpiar.TabIndex = 8;
-            this.btnLimpiar.Text = "Limpiar";
-            this.btnLimpiar.UseVisualStyleBackColor = true;
-            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
-            // 
             // perfilTabla
             // 
             this.perfilTabla.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.perfilTabla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.perfilTabla.Location = new System.Drawing.Point(12, 281);
+            this.perfilTabla.Location = new System.Drawing.Point(455, 111);
             this.perfilTabla.Name = "perfilTabla";
-            this.perfilTabla.Size = new System.Drawing.Size(634, 183);
+            this.perfilTabla.ReadOnly = true;
+            this.perfilTabla.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.perfilTabla.Size = new System.Drawing.Size(634, 396);
             this.perfilTabla.TabIndex = 27;
+            this.perfilTabla.Tag = "nombre";
             this.perfilTabla.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.perfilTabla_RowHeaderMouseClick);
+            this.perfilTabla.SelectionChanged += new System.EventHandler(this.perfilTabla_SelectionChanged);
+            // 
+            // navegador1
+            // 
+            this.navegador1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+            this.navegador1.Location = new System.Drawing.Point(2, 2);
+            this.navegador1.Name = "navegador1";
+            this.navegador1.Size = new System.Drawing.Size(1189, 87);
+            this.navegador1.TabIndex = 28;
+            this.navegador1.Load += new System.EventHandler(this.navegador1_Load);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 20);
+            this.textBox1.TabIndex = 29;
+            this.textBox1.Tag = "pkId";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(144, 176);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 20);
+            this.textBox2.TabIndex = 30;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(361, 274);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(39, 20);
+            this.textBox3.TabIndex = 31;
+            this.textBox3.Tag = "estado";
+            this.textBox3.Visible = false;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // frmMantenimientoPerfil
             // 
-            this.AcceptButton = this.btnInsertar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::CapaVistaSeguridadHSC.Properties.Resources._90_Simple_Backgrounds_Edit_and_Download_Visual_Learning_Center;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(658, 477);
-            this.Controls.Add(this.perfilTabla);
-            this.Controls.Add(this.btnLimpiar);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnModificar);
+            this.ClientSize = new System.Drawing.Size(1191, 519);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.gbxEstado);
-            this.Controls.Add(this.btnInsertar);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.navegador1);
+            this.Controls.Add(this.perfilTabla);
+            this.Controls.Add(this.gbxEstado);
             this.Controls.Add(this.labelNombreaplicacion);
             this.Controls.Add(this.labelIdaplicacion);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -209,19 +180,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.GroupBox gbxEstado;
         private System.Windows.Forms.RadioButton btnInhabilitado;
         private System.Windows.Forms.RadioButton btnHabilitado;
-        private System.Windows.Forms.Button btnInsertar;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label labelNombreaplicacion;
         private System.Windows.Forms.Label labelIdaplicacion;
-        private System.Windows.Forms.Button btnModificar;
-        private System.Windows.Forms.Button btnEliminar;
-        private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.DataGridView perfilTabla;
+        private DLL.nav.navegador navegador1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
