93f9f521d712c6beaf3569ff3996b9b1a5a07c14
kievkeivn13 <kievkevin13@gmail.com>
2022-03-26 20:02:45 -0600

    Contenedor MDI RRHH

---
+++ Modulos/RecursosHumanos/CapaControlador/Controlador.cs
@@ -0,0 +1,13 @@
+﻿using CapaModelo;
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaControlador
+{
+	class Controlador
+	{
+		private Sentencias sn = new Sentencias();
+
+
+	}
+}
---
+++ Modulos/RecursosHumanos/CapaModelo/Sentencias.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.Odbc;
+
+namespace CapaModelo
+{
+	public class Sentencias
+	{
+		private Conexion cn = new Conexion();
+
+
+	}
+}
