3dc01db75da4021e0237a9c227c7471c04ca4cd0
Heydi <sucelyalvarez@hotmail.com>
2022-03-31 18:40:49 -0600

    Implementación de Reportes en Mantenimientos de Pareja 1 - Kevin y Heydi

--- Componentes/Navegador/DLLnav/DLLnav/reporte.Designer.cs
+++ Componentes/Navegador/DLLnav/DLLnav/reporte.Designer.cs
@@ -28,34 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-            this.productos1 = new DLL.nav.productos();
-            this.SuspendLayout();
-            // 
-            // crystalReportViewer1
-            // 
-            this.crystalReportViewer1.ActiveViewIndex = 0;
-            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
-            this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.ReportSource = this.productos1;
-            this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
-            this.crystalReportViewer1.TabIndex = 0;
-            this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load_1);
-            // 
-            // reporte
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.crystalReportViewer1);
-            this.Name = "reporte";
-            this.Text = "reporte";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.reporte_FormClosing_1);
-            this.Load += new System.EventHandler(this.reporte_Load);
-            this.ResumeLayout(false);
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(reporte));
+			this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+			this.productos1 = new DLL.nav.productos();
+			this.SuspendLayout();
+			// 
+			// crystalReportViewer1
+			// 
+			this.crystalReportViewer1.ActiveViewIndex = 0;
+			this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+			this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
+			this.crystalReportViewer1.Name = "crystalReportViewer1";
+			this.crystalReportViewer1.ReportSource = this.productos1;
+			this.crystalReportViewer1.Size = new System.Drawing.Size(800, 450);
+			this.crystalReportViewer1.TabIndex = 0;
+			this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load_1);
+			// 
+			// reporte
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.crystalReportViewer1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "reporte";
+			this.Text = "Reporte Asociado";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.reporte_FormClosing_1);
+			this.Load += new System.EventHandler(this.reporte_Load);
+			this.ResumeLayout(false);
 
         }
 
